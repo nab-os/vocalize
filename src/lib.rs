d65d3cfa7cf53e4dd998e6986c2b5b67dd62f991
Nabos <2191596-nabos@users.noreply.gitlab.com>
2023-04-28 15:41:21 +0200

    Working prototype

---
+++ src/lib.rs
@@ -0,0 +1,198 @@
+use std::{
+    collections::VecDeque,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    InputCallbackInfo,
+};
+
+use rustfft::{num_complex::Complex, num_traits::Pow, FftPlanner};
+
+pub type Raw = Arc<Mutex<VecDeque<f32>>>;
+pub type Frequencies = Arc<Mutex<VecDeque<Option<f32>>>>;
+
+const MAX_FREQUENCIES: usize = 500;
+
+#[derive(Default, Clone)]
+pub struct Vocalize {
+    pub raw: Raw,
+    pub frequencies: Frequencies,
+    pub frequencies_postprocessed: Frequencies,
+}
+
+impl Vocalize {
+    pub fn new() -> Self {
+        let raw: Raw = Arc::new(Mutex::new(VecDeque::new()));
+        let frequencies: Frequencies =
+            Arc::new(Mutex::new(VecDeque::from(vec![None; MAX_FREQUENCIES])));
+        let frequencies_postprocessed: Frequencies =
+            Arc::new(Mutex::new(VecDeque::from(vec![None; MAX_FREQUENCIES])));
+        Vocalize {
+            raw,
+            frequencies,
+            frequencies_postprocessed,
+        }
+    }
+
+    pub fn run(&self) {
+        let host = cpal::default_host();
+        let device = host.default_input_device().unwrap();
+        println!("Input device: {}", device.name().unwrap());
+
+        let config = device.default_input_config().unwrap();
+        println!("Default input config: {:?}", config);
+        println!("Begin recording...");
+
+        println!("Vocalize started");
+        {
+            let raw = self.raw.clone();
+            let frequencies = self.frequencies.clone();
+            let frequencies_postprocessed = self.frequencies_postprocessed.clone();
+            let sample_rate = config.sample_rate().0;
+            thread::spawn(move || {
+                let err_fn = move |err| {
+                    eprintln!("an error occurred on stream: {}", err);
+                };
+                let rw = raw.clone();
+                let stream = match config.sample_format() {
+                    cpal::SampleFormat::F32 => device
+                        .build_input_stream(
+                            &config.clone().into(),
+                            move |data, info| Vocalize::write_input_data(data, info, rw.clone()),
+                            err_fn,
+                            None,
+                        )
+                        .unwrap(),
+                    sample_format => {
+                        eprintln!("Unsupported sample format '{sample_format}'");
+                        return;
+                    }
+                };
+                stream.play().unwrap();
+                loop {
+                    thread::sleep(Duration::from_millis(1000 / 140));
+                    let rw = raw.lock().unwrap();
+                    let sample_size = (sample_rate / 5) as usize;
+                    if rw.len() >= sample_size {
+                        let mut buffer: Vec<Complex<f32>> = rw
+                            .iter()
+                            .rev()
+                            .take(sample_size)
+                            .rev()
+                            .map(|el| Complex {
+                                re: *el,
+                                im: 0.0f32,
+                            })
+                            .collect();
+                        drop(rw);
+
+                        let mut planner = FftPlanner::<f32>::new();
+                        let fft = planner.plan_fft_forward(buffer.len());
+                        fft.process(&mut buffer);
+                        let norms: Vec<(usize, f32)> = buffer
+                            .iter()
+                            .take(buffer.len() / 4)
+                            .map(|freq| ((freq.re * freq.re + freq.im * freq.im) as f32).sqrt())
+                            .enumerate()
+                            .collect();
+
+                        let max_peak = norms.iter().max_by_key(|(_i, norm)| *norm as u32).unwrap();
+
+                        let freq_step = sample_rate as f32 / buffer.len() as f32;
+                        let freq = 2.0 * max_peak.0 as f32 * freq_step;
+                        if max_peak.1 > 5.0 {
+                            println!(
+                                "{:>9}Hz \t\t {:>4}/{:>4} \t\t\t max: {:0<11}",
+                                freq,
+                                max_peak.0 + 1,
+                                buffer.len() / 2,
+                                max_peak.1,
+                            );
+                            let mut fr = frequencies.lock().unwrap();
+                            fr.push_back(Some(freq));
+                        } else {
+                            let mut fr = frequencies.lock().unwrap();
+                            fr.push_back(None);
+                        }
+                    } else {
+                        println!("Sample size too small: {}", rw.len());
+                        let mut fr = frequencies.lock().unwrap();
+                        fr.push_back(None);
+                    }
+
+                    let mut fr = frequencies.lock().unwrap();
+                    while fr.len() > MAX_FREQUENCIES {
+                        fr.pop_front();
+                    }
+                    drop(fr);
+
+                    Vocalize::postprocess(frequencies.clone(), frequencies_postprocessed.clone());
+                }
+            });
+        }
+    }
+
+    pub fn get_values(self) -> VecDeque<Option<f32>> {
+        let fr = self.frequencies_postprocessed.lock().unwrap();
+        fr.clone()
+    }
+
+    fn postprocess(before: Frequencies, after: Frequencies) {
+        let window_size = 5;
+
+        let before = before.lock().unwrap();
+        let count = before
+            .iter()
+            .rev()
+            .take(window_size)
+            .filter(|a| a.is_some())
+            .count();
+
+        let mut after = after.lock().unwrap();
+        after.rotate_left(1);
+        after[before.len() - 1] = before[before.len() - 1];
+        if count > 3 {
+            let sum: f32 = before
+                .iter()
+                .rev()
+                .take(window_size)
+                .filter(|a| a.is_some())
+                .map(|a| a.unwrap())
+                .sum();
+            let mean = sum / count as f32;
+            // let sum_variance: f32 = before
+            //     .iter()
+            //     .rev()
+            //     .take(window_size)
+            //     .filter(|a| a.is_some())
+            //     .map(|a| {
+            //         let a = a.unwrap();
+            //         (a - mean).pow(2)
+            //     })
+            //     .sum();
+            // let mean_variance = sum_variance / ((count - 1) as f32);
+            // let standard_deviation = mean_variance.sqrt();
+
+            if let Some(_a) = before[before.len() - 1] {
+                // let centered_reduced = (a - mean) / standard_deviation;
+                after[before.len() - 1] = Some(mean);
+                // if standard_deviation > 2000.0 {
+                //     after[before.len() - 1] = None;
+                // }
+            }
+        }
+    }
+
+    fn write_input_data(input: &[f32], _info: &InputCallbackInfo, raw: Raw) {
+        let mut temp: VecDeque<f32> = input.to_vec().into();
+        let mut rw = raw.lock().unwrap();
+        rw.append(&mut temp);
+        while rw.len() > 200000 {
+            rw.pop_front();
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,133 @@
+#![allow(non_snake_case)]
+use std::time::Duration;
+
+use dioxus::prelude::*;
+
+use tokio::time::sleep;
+use vocalize::Vocalize;
+
+const MAX: f32 = 700.0;
+
+fn main() {
+    dioxus_desktop::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    let svg_style = r"
+        width: 100vw;
+        height: 100vh;
+    ";
+
+    let lines = use_state(cx, || Vec::new());
+    use_coroutine(cx, |_: UnboundedReceiver<()>| {
+        let lines = lines.to_owned();
+        async move {
+            let vocalize = Box::new(Vocalize::new());
+            vocalize.run();
+            loop {
+                sleep(Duration::from_millis(1000 / 30)).await;
+                lines.set(
+                    vocalize
+                        .clone()
+                        .get_values()
+                        .iter()
+                        .enumerate()
+                        .fold(Vec::<Vec<(usize, String)>>::new(), |mut acc, (i, v)| {
+                            if acc.is_empty() {
+                                acc.push(Vec::<(usize, String)>::new())
+                            }
+                            match v {
+                                Some(v) => acc.last_mut().unwrap().push((i, (MAX - v).to_string())),
+                                None => acc.push(Vec::<(usize, String)>::new()),
+                            }
+                            acc
+                        })
+                        .iter()
+                        .map(|l| {
+                            let temp: Vec<String> =
+                                l.iter().map(|(a, b)| format!("{} {}", a, b)).collect();
+                            temp.join(" L")
+                        })
+                        .collect(),
+                );
+            }
+        }
+    });
+
+    cx.render(rsx! {
+        svg {
+            style: svg_style,
+            preserve_aspect_ratio: "xMidYMid meet",
+            fill: "none",
+            stroke: "red",
+            xmlns: "http://www.w3.org/2000/svg",
+            "viewBox": "0 0 600 {MAX}",
+
+            //Back
+            g {
+                rect {
+                    x: "0",
+                    y: "{MAX - 180.0 - 125.0}",
+                    width: "600",
+                    height: "125",
+                    style: "fill: rgb(245 169 184); stroke: none",
+                },
+                rect {
+                    x: "0",
+                    y: "{MAX - 80.0 - 80.0}",
+                    width: "600",
+                    height: "85",
+                    style: "fill: rgb(91 206 250); stroke: none",
+                },
+
+                for y in (50..450).step_by(50) {
+                    rsx! {
+                        line {
+                            x1: "0",
+                            y1: "{MAX - y as f32}",
+                            x2: "600",
+                            y2: "{MAX - y as f32}",
+                            style: "stroke: gray; stroke-with: 1;",
+                        },
+                        text {
+                            x: "570",
+                            y: "{MAX - y as f32 - 2.0}",
+                            style: "fill: gray; stroke: none",
+                            y.to_string(),
+                        }
+                    }
+                }
+            }
+
+            //Middle
+            g {
+                lines.iter().map(|l|
+                    rsx!{
+                        path {
+                            "stroke-linecap": "round",
+                            "stroke-width": "2",
+                            "stroke-linejoin": "round",
+                            d: "M{l.clone()}",
+                        }
+                    }
+                )
+            }
+
+            //Front
+            g {
+                text {
+                    x: "5",
+                    y: "{MAX - 180.0 - 125.0 + 33.0}",
+                    style: "fill: white; stroke: none",
+                    "Hauteur féminine"
+                }
+                text {
+                    x: "5",
+                    y: "{MAX - 80.0 - 85.0 + 47.0}",
+                    style: "fill: white; stroke: none",
+                    "Hauteur masculine"
+                }
+            }
+        }
+    })
+}
